d9f0f225416d8f6afb6e2e5064b958b4f99fe9be
Zvonimir Korajlija <zvonimir.korajlija@gmail.com>
2024-06-12 15:01:12 +0200

    - Added osc matching tool feature
    (cherry picked from commit 5bf257c4de8c7772d006650065df1cc75345b442)

---
+++ Rock.ViewModels/Blocks/OscMatching/MatchingTool/OnSiteCoordinatorAttributeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Rock.ViewModels.Blocks.OscMatching.MatchingTool
+{
+    internal class OnSiteCoordinatorAttributeModel
+    {
+        public string CampusGuid { get; set; }
+        public string DayPreference { get; set; }
+        public string TimePreference { get; set; }
+        public string ExtraInfo { get; set; }
+        public string MaxProjects { get; set; }
+    }
+}
---
+++ Rock.ViewModels/Blocks/OscMatching/MatchingTool/OnSiteCoordinatorProjectModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Rock.ViewModels.Blocks.OscMatching.MatchingTool
+{
+    public class OnSiteCoordinatorProjectModel
+    {
+        public int PersonId { get; set; }
+        public int ProjectId { get; set; }
+        public Guid ProjectGuid { get; set; }
+    }
+}
