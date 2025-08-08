ddfff154e46ee4b5d9f99a279aa2a7d4c573751f
kylehenning <kylebhenning@gmail.com>
2025-08-08 10:14:02 -0700

    - Updated eRA Migration job to be a scheduled job

--- Rock.Migrations/RockStartup/DataMigrationsStartup.cs
+++ Rock.Migrations/RockStartup/DataMigrationsStartup.cs
@@ -128,7 +128,8 @@ namespace Rock.Migrations.RockStartup
             SystemGuid.ServiceJob.DATA_MIGRATIONS_133_ADD_INTERACTION_SESSION_INTERACTION_SESSION_LOCATION_ID_INDEX.AsGuid(),
             SystemGuid.ServiceJob.POST_170_UPDATE_HISTORY_ENTITYTYPEID_INDEX.AsGuid(),
             SystemGuid.ServiceJob.DATA_MIGRATIONS_173_UPDATE_NAMELESS_SCHEDULES.AsGuid(),
-            SystemGuid.ServiceJob.DATA_MIGRATIONS_180_UPDATE_NAMELESS_LOCATIONS.AsGuid()
+            SystemGuid.ServiceJob.DATA_MIGRATIONS_180_UPDATE_NAMELESS_LOCATIONS.AsGuid(),
+            SystemGuid.ServiceJob.DATA_MIGRATIONS_180_MIGRATE_ERA_DATA_TO_STEPS.AsGuid()
         };
 
         /// <summary>
