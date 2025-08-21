6367bfe5678e84cf4ba66152c8eedda6232bc37e
Joshua Henninger <joshua@triumph.tech>
2025-08-21 16:05:37 -0700

    - Fixed Obsidian build warning.

--- Rock.JavaScript.Obsidian/Framework/Libs/chart.ts
+++ Rock.JavaScript.Obsidian/Framework/Libs/chart.ts
@@ -2,12 +2,12 @@ export * from "chart.js/auto";
 export type { AnnotationPluginOptions } from "chartjs-plugin-annotation";
 
 import { Chart } from "chart.js/auto";
+
 import annotationPlugin from "chartjs-plugin-annotation";
-import ChartDataLabels from "chartjs-plugin-datalabels";
-import { Options as ChartDataLabelsOptions, LabelOptions as ChartDataLabelOptions } from "chartjs-plugin-datalabels/types/options";
+Chart.register(annotationPlugin);
+
 import "chartjs-adapter-luxon";
 
-Chart.register(annotationPlugin);
-//Chart.register(ChartDataLabels); // Register plugin in charts that need it.
+export { default as ChartDataLabels } from "chartjs-plugin-datalabels";
+export type { Options as ChartDataLabelsOptions, LabelOptions as ChartDataLabelOptions } from "chartjs-plugin-datalabels/types/options";
 
-export { ChartDataLabels, type ChartDataLabelsOptions, type ChartDataLabelOptions };
